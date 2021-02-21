9e9477ba5b9aa433141d8a54adc7697824f70c8d
Kenny Boyer <ksboyerii@gmail.com>
2021-02-21 14:29:42 -0600

    added addition html structure and some css

---
+++ script.js
@@ -0,0 +1,15 @@
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+//construction of the quiz
+function buildQuiz(){}
+
+function showResults(){}
+
+//displays results immediately
+buildQuiz();
+
+
+//listening for the submission of the quiz
+submitButton.addEventListener('click', showResults);
